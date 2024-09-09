5e6114a808d6c79cb88dd8cdeea84e8221a9ac95
chizuru1227 <1227chizuru@ezweb.ne.jp>
2024-09-09 11:03:35 +0900

    add storage backend

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,3 +1,27 @@
+<template>
+  <main>
+    <h1>My todos</h1>
+    <button @click="createTodo">+ new</button>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
+        {{ todo.content }}
+      </li>
+    </ul>
+    <div>
+      🥳 App successfully hosted. Try creating a new todo.
+      <br />
+      <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
+        Review next steps of this tutorial.
+      </a>
+    </div>
+    <div>
+      <p>ファイルをここにドラッグ＆ドロップしてください</p>
+      <input type="file" ref="fileInput" @change="handleFileChange" />
+    </div>
+  </main>
+</template>
+
+
 <script setup lang="ts">
 import '@/assets/main.css';
 import { onMounted, ref } from 'vue';
@@ -13,8 +37,8 @@ function listTodos() {
   client.models.Todo.observeQuery().subscribe({
     next: ({ items, isSynced }) => {
       todos.value = items
-     },
-  }); 
+    },
+  });
 }
 
 function createTodo() {
@@ -25,31 +49,81 @@ function createTodo() {
     listTodos();
   });
 }
-    
+
+function deleteTodo(id: string) {
+  client.models.Todo.delete({ id })
+}
+
 // fetch todos when the component is mounted
- onMounted(() => {
+onMounted(() => {
   listTodos();
 });
 
-</script>
 
-<template>
-  <main>
-    <h1>My todos</h1>
-    <button @click="createTodo">+ new</button>
-    <ul>
-      <li 
-        v-for="todo in todos" 
-        :key="todo.id">
-        {{ todo.content }}
-      </li>
-    </ul>
-    <div>
-      🥳 App successfully hosted. Try creating a new todo.
-      <br />
-      <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
-        Review next steps of this tutorial.
-      </a>
-    </div>
-  </main>
-</template>
+// 追加部分
+import { uploadData } from "aws-amplify/storage";
+
+const fileInput = ref<HTMLInputElement | null>(null);
+
+function handleDrop(event: DragEvent) {
+  const files = event.dataTransfer?.files;
+  if (files && files.length) {
+    uploadFile(files[0]);
+  }
+}
+
+function handleFileChange(event: Event) {
+  const input = event.target as HTMLInputElement;
+  if (input.files && input.files.length) {
+    uploadFile(input.files[0]);
+  }
+}
+
+async function uploadFile(fileData: File) {
+  const reader = new FileReader();
+  reader.readAsDataURL(fileData);
+  reader.onload = async (e) => {
+    console.log("fileData", fileData);
+    console.log("fileName", fileData.name);
+
+    // ファイル名からスペース除去(これはいずれutilに)
+    let noSpaces = Array.from(fileData.name)
+      .filter((char) => char !== " ")
+      .join("");
+    console.log("noSpaces", noSpaces);
+
+    const file = {
+      data: fileData,
+      filePath: noSpaces,
+    };
+
+    try {
+      const result = await uploadData({
+        data: file.data,
+        path: `test/123456/${file.filePath}`,
+      });
+      console.log("result", result);
+    } catch (e) {
+      console.log("error", e);
+    }
+  };
+
+  /*
+  
+    try {
+      const response = await fetch('/api/upload', {
+        method: 'POST',
+        body: formData,
+      })
+  
+      if (!response.ok) {
+        throw new Error('アップロードに失敗しました')
+      }
+  
+      console.log('アップロード成功')
+    } catch (error) {
+      console.error(error)
+    }
+      */
+}
+</script>
